25c8e74bb8c7fda7297493f9b9d30fd332639d9d
Brian Maitho <Bmaitho@gmail.com>
2024-03-19 10:09:14 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1*num2);
+
+let random = Math.floor(Math.random() * 10);
+console.log(random)
+let num3 = 7
+let num4 = 39
+let mod = num4%num3
+
+let max = Math.max(4, 15, 20);
